711177a9d772135cbefeeea19126600f2525c05a
ayushgosi <anime.ayush@gmail.com>
2016-06-15 01:23:29 +0530

    Added back button to rawadta page

--- js/app.js
+++ js/app.js
@@ -290,3 +290,19 @@ populationHack.controller('rawdataController', ['$scope', '$resource', 'populati
   console.log(rawdata.data);
 
 }]);
+
+//back to previous url module
+populationHack.run(function ($rootScope, $location) {
+
+    var history = [];
+
+    $rootScope.$on('$routeChangeSuccess', function() {
+        history.push($location.$$path);
+    });
+
+    $rootScope.back = function () {
+        var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
+        $location.path(prevUrl);
+    };
+
+});
